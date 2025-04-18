0c905d4856aad8f760f7c353a7b65f80866a0bdf
richarlisooon <smorodindima12@gmail.com>
2025-04-18 20:11:31 +0300

    repos full

--- plane-service/src/cpp/repos/AirportRepository.cpp
+++ plane-service/src/cpp/repos/AirportRepository.cpp
@@ -10,16 +10,87 @@ list<AirportModel> AirportRepository::getAirports(long int* id, string* name, in
 {
     Client cli(DATABASE_SERVICE_HOST_PORT);
 
+    Headers headers = {
+        {SERVICE_TOKEN_NAME, SERVICE_TOKEN_VALUE}
+    };
+    Params params;
+    if (id != nullptr)
+        params.insert({"id", to_string(*id)});
+    if (name != nullptr)
+        params.insert({"name", *name});
+    if (size != nullptr)
+        params.insert({"size", to_string(*size)});
+    if (x != nullptr)
+        params.insert({"x", to_string(*x)});
+    if (y != nullptr)
+        params.insert({"y", to_string(*y)});
+    auto res = cli.Get(DATABASE_AIRPORT_GET_MAPPING, params, headers);
+    if (res->status >= 200 && res->status < 300)
+    {
+        json airports = json::parse(res->body);
+        list<AirportModel> result;
+        for (auto item : airports)
+        {
+            AirportModel airport(item["id"], item["name"], item["size"], item["x"], item["y"]);
+            result.push_back(airport);
+        }
+        return result;
+    }
+    throw res->status;
 }
 AirportModel AirportRepository::createAirport(AirportModel airport)
 {
     Client cli(DATABASE_SERVICE_HOST_PORT);
 
+    Headers headers = {
+        { SERVICE_TOKEN_NAME, SERVICE_TOKEN_VALUE }
+    };
+    json airport_json;
+    airport_json["id"] = airport.getId();
+    airport_json["name"] = airport.getName();
+    airport_json["size"] = airport.getSize();
+    airport_json["x"] = airport.getX();
+    airport_json["y"] = airport.getY();
+
+    auto res = cli.Post(DATABASE_AIRPORT_CREATE_MAPPING, headers, airport_json.dump(), "application/json");
+    if (res->status >= 200 && res->status < 300)
+    {
+        json airport_json = json::parse(res->body);
+        AirportModel airport(airport_json["id"], airport_json["name"], airport_json["size"], airport_json["x"], airport_json["y"]);
+        return airport;
+    }
+
+    throw res->status;
 }
 AirportModel AirportRepository::updateAirport(AirportModel airport, set<string> updates)
 {
     Client cli(DATABASE_SERVICE_HOST_PORT);
+    Headers headers = {
+        { SERVICE_TOKEN_NAME, SERVICE_TOKEN_VALUE }
+    };
 
+    json airport_json;
+    airport_json["id"] = airport.getId();
+    airport_json["name"] = airport.getName();
+    airport_json["size"] = airport.getSize();
+    airport_json["x"] = airport.getX();
+    airport_json["y"] = airport.getY();
+    string update;
+    for (auto item : updates)
+    {
+        if (update.empty())
+            update = item;
+        else
+            update += "," + item;
+    }
+    auto res = cli.Post((DATABASE_AIRPORT_UPDATE_MAPPING + "?update=" + update), headers, airport_json.dump(), "application/json");
+    if (res->status >= 200 && res->status < 300)
+    {
+        json airport_json = json::parse(res->body);
+        AirportModel airport(airport_json["id"], airport_json["name"], airport_json["size"], airport_json["x"], airport_json["y"]);
+        return airport;
+    }
+    throw res->status;
 }
 
 
@@ -27,5 +98,11 @@ bool AirportRepository::deleteAirport(long int id)
 {
     Client cli(DATABASE_SERVICE_HOST_PORT);
 
-
+    Headers headers = {
+    { SERVICE_TOKEN_NAME, SERVICE_TOKEN_VALUE }
+    };
+    auto res = cli.Delete(DATABASE_AIRPORT_DELETE_MAPPING + "/" + to_string(id), headers);
+    if (res->status >= 200 && res->status < 300)
+        return true;
+    throw res->status;
 }
--- plane-service/src/cpp/repos/FlightRepository.cpp
+++ plane-service/src/cpp/repos/FlightRepository.cpp
@@ -10,13 +10,58 @@ list<FlightModel> FlightRepository::getFlights(long int* id, long int* timestamp
 {
     Client cli(DATABASE_SERVICE_HOST_PORT);
 
-
+    Headers headers = {
+        { SERVICE_TOKEN_NAME, SERVICE_TOKEN_VALUE }
+    };
+    Params params;
+    if (id != nullptr)
+        params.insert(make_pair("id", to_string(*id)));
+    if (timestampStart != nullptr)
+        params.insert(make_pair("timestampStart", to_string(*timestampStart)));
+    if (timestampEnd != nullptr)
+        params.insert(make_pair("timestampEnd", to_string(*timestampEnd)));
+    if (dispatcherId != nullptr)
+        params.insert(make_pair("dispatcherId", to_string(*dispatcherId)));
+    if (planeId != nullptr)
+        params.insert(make_pair("planeId", to_string(*planeId)));
+    if (airportId != nullptr)
+        params.insert(make_pair("airportId", to_string(*airportId)));
+    auto res = cli.Get(DATABASE_FLIGHT_GET_MAPPING, params, headers);
+    if (res->status >= 200 && res->status < 300)
+    {
+        json flights_json = json::parse(res->body);
+        list<FlightModel> flights;
+        for (auto item : flights_json)
+        {
+            FlightModel flight(item["id"], item["timestampStart"], item["timestampEnd"], item["dispatcherId"], item["planeId"], item["airportId"]);
+            flights.push_back(flight);
+        }
+        return flights;
+    }
+    throw res->status;
 }
 
 FlightModel FlightRepository::createFlight(FlightModel flight)
 {
     Client cli(DATABASE_SERVICE_HOST_PORT);
 
-
+    Headers headers = {
+        { SERVICE_TOKEN_NAME, SERVICE_TOKEN_VALUE }
+    };
+    json flight_json;
+    flight_json["id"] = flight.getId();
+    flight_json["timestampStart"] = flight.getTimestampStart();
+    flight_json["timestampEnd"] = flight.getTimestampEnd();
+    flight_json["dispatcherId"] = flight.getDispatcherId();
+    flight_json["planeId"] = flight.getPlaneId();
+    flight_json["airportId"] = flight.getAirportId();
+    auto res = cli.Post(DATABASE_FLIGHT_CREATE_MAPPING, headers, flight_json.dump(), "application/json");
+    if (res->status >= 200 && res->status < 300)
+    {
+        json flights_json = json::parse(res->body);
+        FlightModel flight(flights_json["id"], flights_json["timestampStart"], flights_json["timestampEnd"], flights_json["dispatcherId"], flights_json["planeId"], flights_json["airportId"]);
+        return flight;
+    }
+    throw res->status;
 }
 
